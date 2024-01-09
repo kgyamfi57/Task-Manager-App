abbec1ffbdd8251a00cf3d3353bb59b624169de5
Raiyan Hossain <raiyanhossain123@yahoo.com>
2024-01-09 18:27:42 +0000

    Added a Task.java class. * added the attributes id, title, description, dueDate, priority and status. * generated constructor. * generated setters and getters methods.

---
+++ src/main/java/com/taskmanagerapp/Task.java
@@ -0,0 +1,62 @@
+package com.taskmanagerapp;
+
+public class Task {
+    private int id;
+    private String title;
+    private String description;
+    private int dueDate;
+    private int priority;
+    private int status;
+
+    public Task(String title, String description, int dueDate, int priority, int status) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(int dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+}
