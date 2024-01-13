e4896283b1b3fbff195f5f7d93774d9552c3f9a9
Raiyan Hossain <raiyanhossain123@yahoo.com>
2024-01-13 01:19:12 +0000

    Update TaskMain.java.
    * Updated the main() method using FileWriter to persist the task details into a txt file.
    * Added formatTaskDetails() method
    * Added getStatusLabel() method.

--- src/main/java/com/taskmanagerapp/TaskMain.java
+++ src/main/java/com/taskmanagerapp/TaskMain.java
@@ -1,9 +1,36 @@
 package com.taskmanagerapp;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class TaskMain {
     public static void main(String[] args) {
 
         TaskManager taskManager = new TaskManager();
+        try (FileWriter writer = new FileWriter("output.txt")) {
+            String taskDetails = formatTaskDetails(taskManager);
+            writer.write(taskDetails);
+            System.out.println("Text saved to file successfully!");
+        } catch (IOException e) {
+            System.out.println("Error writing to file: " + e.getMessage());
+        }
 
+    }
+
+    private static String formatTaskDetails(TaskManager taskManager){
+        return "\nTitle: " + taskManager.getTitle() +
+                "\nDescription: " + taskManager.getDescription() +
+                "\nDue Date: " + taskManager.getDueDate() +
+                "\nPriority: " + taskManager.getPriority() +
+                "\nStatus: " + getStatusLabel(taskManager.getStatus());
+    }
+
+    private static String getStatusLabel(int status) {
+        return switch (status) {
+            case 1 -> "TO DO";
+            case 2 -> "IN PROGRESS";
+            case 3 -> "COMPLETED";
+            default -> "Invalid Status";
+        };
     }
 }
